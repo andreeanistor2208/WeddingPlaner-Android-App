3732dc459e1946b7a34c7ebadfaaaa6813fe9f67
Andreea Nistor <andreea.nistor2208@stud.acs.upb.ro>
2022-05-23 21:33:51 +0300

    date picker done

--- app/src/main/java/com/example/myapplication/DashboardActivity.kt
+++ app/src/main/java/com/example/myapplication/DashboardActivity.kt
@@ -1,11 +1,62 @@
 package com.example.myapplication
 
+import android.app.DatePickerDialog
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.DatePicker
+import android.widget.TextView
+import com.example.myapplication.databinding.ActivityDashboardBinding
+import com.example.myapplication.databinding.ActivityMainBinding
+import java.text.SimpleDateFormat
+import java.util.*
 
 class DashboardActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityDashboardBinding
+
+    private lateinit var tvDatePicker: TextView
+
+    private lateinit var buttonDate: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_dashboard)
+        //setContentView(R.layout.activity_dashboard)
+
+        binding = ActivityDashboardBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        tvDatePicker = findViewById(R.id.tvDate)
+        buttonDate = findViewById(R.id.buttonDate)
+
+        val myCalendar = Calendar.getInstance()
+
+        val datePicker = DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
+            myCalendar.set(Calendar.YEAR, year)
+            myCalendar.set(Calendar.MONTH, month)
+            myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+            updateLable(myCalendar)
+        }
+
+        binding.button.setOnClickListener {
+
+            val intent = Intent(this, MainActivity::class.java);
+            startActivity(intent);
+
+        }
+
+        binding.buttonDate.setOnClickListener {
+            DatePickerDialog(this, datePicker, myCalendar.get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+            myCalendar.get(Calendar.DAY_OF_MONTH)).show()
+        }
+
+
+    }
+
+    private fun updateLable(myCalendar: Calendar) {
+        val myFormat = " dd-MM-yyyy"
+        val sdf = SimpleDateFormat(myFormat, Locale.UK)
+        tvDatePicker.setText(sdf.format(myCalendar.time))
+
     }
 }
